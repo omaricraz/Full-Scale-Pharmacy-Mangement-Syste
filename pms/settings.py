0847c312666a898e22e53a3a64afbdec9cd89205
Hamse Mohamed <you@example.com>
2025-06-14 11:11:48 +0300

    modified setting

--- pms/settings.py
+++ pms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-c8ltyp7kmk14kgw$to-1v%@z9r3_5gn+=5supdd1%k9r^vzgay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
